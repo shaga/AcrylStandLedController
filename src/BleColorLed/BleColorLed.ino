1194692934cd3bac4345123b0d423781607e954f
shaga <shohaga@gmail.com>
2019-06-04 14:16:57 +0900

    ble server charactersitic update * changing color

--- src/BleColorLed/BleColorLed.ino
+++ src/BleColorLed/BleColorLed.ino
@@ -1,17 +1,52 @@
-#include <Adafruit_NeoPixel.h>
+#include <NeoPixelAnimator.h>
+#include <NeoPixelBrightnessBus.h>
+#include <NeoPixelBus.h>
+
+//#include <Adafruit_NeoPixel.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
+#include <esp_spi_flash.h>
 
 #define UUID_LED_SERVICE        "5BDC13B0-F954-43D3-939B-4F701FFD80D8"
 #define UUID_LED_CHAR           "5BDC13B1-F954-43D3-939B-4F701FFD80D8"
 
 static const int PinLed = 14;
 static const int NumLeds = 5;
+static const int TickLengthMs = 200;
+static const uint32_t ColorDataSpiAddress = 0x100000;
+static const uint8_t WriteFlagValue = 0x55;
+static const uint8_t DefStartCoor[] = {0x00, 0x00, 0xFF};
+static const uint8_t DefEndColor[] = {0x00, 0x00, 0xFF};
+static const uint8_t DefChangeCount = 50;
+static const uint8_t DefTurnTickCount = 6;
 
-static uint8_t colors[] = {0, 0, 0};
-Adafruit_NeoPixel pixels(NumLeds, PinLed, NEO_GRB + NEO_KHZ800);
+static void initColorData();
+static void updateColorData(uint8_t *buffer);
+static void taskChangeColor(void* parameters);
+
+typedef struct {
+    uint8_t change_count;
+    uint8_t start_col[3];
+    uint8_t end_col[3];
+    uint8_t buf;
+} ColorData_t;
+
+static uint8_t colors[] = {0, 0, 0xff};
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>  pixels(NumLeds, PinLed);
+static BLECharacteristic *my_characteristic = NULL;
+static BLEServer *server = NULL;
+
+static uint32_t change_pos = 0;
+static int32_t change_dir = 1;
+static uint32_t change_slow_pow = 0;
+TaskHandle_t handle_task_change_color = NULL;
+SemaphoreHandle_t handle_semaphore = NULL;
+
+static ColorData_t color_data;
 
 class MyBleCallbacks : public BLEServerCallbacks {
 public:
@@ -19,15 +54,16 @@ public:
   
   void onConnect(BLEServer *server) {
     is_connected = true;
+    if (server != NULL) server->getAdvertising()->stop();
   }
 
   void onDisconnect(BLEServer *server) {
     is_connected = false;
+    if (server != NULL) server->getAdvertising()->start();
   }
 };
-
 bool MyBleCallbacks::is_connected = false;
-static BLECharacteristic *my_characteristic = NULL;
+
 
 class MyBleCharCallbacks : public BLECharacteristicCallbacks {
 public:
@@ -35,20 +71,14 @@ public:
     static bool is_updated;
 
     void onWrite(BLECharacteristic *characteristic) {
-        Serial.println("fuga");
         if (is_updated) return;
 
         int len = characteristic->getValue().length();
-        Serial.print("hoge:");
-        Serial.println(len);
-        if (len != 3) return;
+        if (len != 7) return;
 
         uint8_t *data = characteristic->getData();
-        colors[0] = data[0];
-        colors[1] = data[1];
-        colors[2] = data[2];
 
-        is_updated = true;
+        updateColorData(data);   
     }
 
     void onRead(BLECharacteristic *characteristic) {
@@ -59,18 +89,96 @@ public:
 bool MyBleCharCallbacks::is_updated = false;
 
 void setLedColor() {
-    pixels.clear();
+    //pixels.Clear();
 
     for (int i = 0; i < NumLeds; i++) {
-        pixels.setPixelColor(i, pixels.Color(colors[0], colors[1], colors[2]));
-    pixels.show();
+        pixels.SetPixelColor(i,RgbColor(colors[0], colors[1], colors[2]));
+        delay(1);
+        pixels.Show();
+    }
+}
+static void initColorData() {
+    color_data.change_count = DefChangeCount;
+    for (int i = 0; i < 3; i++) {
+        color_data.start_col[i] = DefStartCoor[i];
+        color_data.end_col[i] = DefEndColor[i];
+    }
+}
+
+static void updateColorData(uint8_t *buffer) {
+    bool no_change = true;
+
+    while(xSemaphoreTake(handle_semaphore, (TickType_t)(50 * portTICK_RATE_MS)) == pdTRUE);
+
+    if (buffer != NULL) {
+        memcpy(&color_data, buffer, 7);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (color_data.start_col[i] != color_data.end_col[i]) {
+            no_change = false;
+            break;
+        }
+    }
+
+    if (!no_change) {
+        change_pos = 0;
+
+        if (handle_task_change_color == NULL) {
+            xTaskCreatePinnedToCore(taskChangeColor, "hoge", 4096, NULL, 1, &handle_task_change_color, 0);
+        }        
+    } else {
+        if (handle_task_change_color != NULL) {
+            vTaskDelete(handle_task_change_color);
+            handle_task_change_color = NULL;
+        }
+
+        memcpy(colors, color_data.start_col, 3);
+
+        setLedColor();
+    }
+
+    xSemaphoreGive(handle_semaphore);
+}
+
+static void taskChangeColor(void* parameters) {
+    const TickType_t TickLength = (TickType_t)(TickLengthMs * portTICK_RATE_MS);
+    TickType_t tick = xTaskGetTickCount();
+    
+    while(true) {
+        if (handle_semaphore != NULL && xSemaphoreTake(handle_semaphore, (TickType_t)(100 * portTICK_RATE_MS)) == pdTRUE) {
+            for (int i = 0; i < 3; i++) {
+                if (color_data.end_col[i] > color_data.start_col[i]) {
+                    colors[i] = color_data.start_col[i] + (color_data.end_col[i] - color_data.start_col[i]) * change_pos / color_data.change_count;
+                } else if (color_data.end_col[i] < color_data.start_col[i]) {
+                    colors[i] = color_data.start_col[i] - (color_data.start_col[i] - color_data.end_col[i]) * change_pos / color_data.change_count;                  
+                } else {
+                    colors[i] = color_data.start_col[i];
+                }
+            }
+
+            setLedColor();
+            if (change_pos == 0 && change_dir < 0) {
+                change_dir = 1;
+            } else if (change_pos == color_data.change_count && change_dir > 0) {
+                change_dir = -1;
+            }
+
+            change_pos += change_dir;
+
+            xSemaphoreGive(handle_semaphore);
+            vTaskDelayUntil(&tick, TickLength);
+        } else {
+            Serial.print("sem:");
+            Serial.println(handle_semaphore != NULL);
+        }
     }
 }
 
 void initBle() {
     BLEDevice::init("LED_CONTROLLER");
 
-    BLEServer *server = BLEDevice::createServer();
+    server = BLEDevice::createServer();
     server->setCallbacks(new MyBleCallbacks);
     BLEDevice::setEncryptionLevel((esp_ble_sec_act_t)ESP_LE_AUTH_REQ_SC_BOND);
 
@@ -81,7 +189,7 @@ void initBle() {
         BLECharacteristic::PROPERTY_READ |
         BLECharacteristic::PROPERTY_WRITE_NR);
     
-    my_characteristic->setValue(colors, 3);
+    my_characteristic->setValue((uint8_t*)&color_data, 7);
     my_characteristic->setCallbacks(new MyBleCharCallbacks());
 
     service->start();
@@ -98,18 +206,21 @@ void initBle() {
 
 void setup() {
     Serial.begin(115200);
-    pixels.begin();
-    delay(100);
-    setLedColor();
-    
+    pixels.Begin();
+
+    handle_semaphore = xSemaphoreCreateBinary();
+    if (handle_semaphore != NULL) {
+        while(xSemaphoreGive(handle_semaphore) != pdTRUE);
+    }
+
+    initColorData();
+
     initBle();
+
+    updateColorData(NULL);    
 
     Serial.println("start>>>");
 }
 
 void loop() {
-    if (!MyBleCharCallbacks::is_updated) return;
-    Serial.println("update");
-    setLedColor();
-    MyBleCharCallbacks::is_updated = false;
 }
